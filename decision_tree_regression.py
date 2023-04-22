0f33eae9531ba721563cfa12a40807fd470d84fd
Charanvir <charanvir123@gmail.com>
2023-04-22 18:17:56 -0400

    implemented visualization of hte DTR

--- decision_tree_regression.py
+++ decision_tree_regression.py
@@ -11,3 +11,12 @@ y = dataset.iloc[:, -1].values
 regressor = DecisionTreeRegressor(random_state=0)
 regressor.fit(x, y)
 prediction = regressor.predict([[6.5]])
+
+x_grid = np.arange(min(x), max(x), 0.1)
+x_grid = x_grid.reshape(len(x_grid), 1)
+plt.scatter(x, y, color="red")
+plt.plot(x_grid, regressor.predict(x_grid), color="blue")
+plt.title("Decision Tree Regression Model")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
