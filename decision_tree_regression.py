840cd485b4a80df9f7cd84e533bf1b52b1f7853a
Charanvir <charanvir123@gmail.com>
2023-04-22 18:05:55 -0400

    Created a decision tree regression model and trained to with the entire sample dataset

--- decision_tree_regression.py
+++ decision_tree_regression.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor
 
 dataset = pd.read_csv("Position_Salaries.csv")
 x = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
+# Training the Decision Tree Regression model on the whole dataset
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(x, y)
+prediction = regressor.predict([[6.5]])
